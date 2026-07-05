82ced2e2bad52a6bfe1a57f174db505ef4c0465e
khushi335 <skhushi0218@gmail.com>
2026-07-05 11:22:11 -0400

    feat(routing): register application views and URL rules for service matrix

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("",index,name="index"),
-    path("about/",about,name="about")
+    path("about/",about,name="about"),
+    path("service/",service,name="service"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"main/index.html")
+    
+def about(request):
+    return render(request,"main/about.html")
+    
+def service(request):
+    return render(request,"main/service.html")
